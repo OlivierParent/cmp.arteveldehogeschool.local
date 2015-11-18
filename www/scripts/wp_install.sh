c5b578341145ac752a4ab9ae79ef4268b6aa78fc
Olivier Parent <olivier.parent@arteveldehs.be>
2015-11-18 22:07:09 +0100

    [WIP]

--- www/scripts/wp_install.sh
+++ www/scripts/wp_install.sh
@@ -6,4 +6,4 @@ cd `dirname $0`/..
 wp core download --path=${WP_PATH} --locale=${WP_LOCALE}
 cd ${WP_PATH}
 wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX}
-wp core install --url=${WP_URL} --title='${COURSE}' --admin_user=${WP_USER} --admin_password=${WP_PASS} --admin_email=${WP_MAIL}
+wp core install --url=${WP_URL} --title="${COURSE}" --admin_user=${WP_USER} --admin_password=${WP_PASS} --admin_email=${WP_MAIL}
--- www/wordpress/wp-config.php
+++ www/wordpress/wp-config.php
@@ -20,14 +20,14 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('AUTH_KEY',         'wP.8+Xlr6;6VV.$WPQ<0d4o8h cv|ZW7c3Jq8e(aOC;/kaP }M-ga82^M|#f84oT');
-define('SECURE_AUTH_KEY',  'va2I8f(1r)0g-@imcG+*UM;TSy{tz.-M#rP_fx/t9G6#Ft~O9Xq  <d<9hLpNTUU');
-define('LOGGED_IN_KEY',    '~h/Cm*Y4:a~Su?YR}b>CKhY.q+vN,-(xaSLnmaP=7VmZXV6iBh0K>UB?9e%r9|8g');
-define('NONCE_KEY',        '(oIK5K%ox.Ux]G }P+r4/}[pTj2H)x?F~w?M3djiU:p!q:QSsbi:=d+p|?kHh<,Y');
-define('AUTH_SALT',        '#|-9v%M;|9]%I<6T;mDPIrZbMA+mz[IMWsrAV5?6J Jx~n2=aSSE13*dv7_Fz-%1');
-define('SECURE_AUTH_SALT', 'F*0+-+V]2@W#fG~2bHI0]U5i;E}`-eFK2DAoE*1K 35]h6:y[]:}=,|VJ`vLx(7k');
-define('LOGGED_IN_SALT',   'Kyi|esg<jK%(ePnK^q{CY/}AAKecHaS*aS7|)6-rnGHkBKXhh-*9t#=A@-wn-`uo');
-define('NONCE_SALT',       '-lE=uWAZ;QP1|g=Kx8Z8Ma{`{e:mkO#Wv*N|Z[}NTSh|8yti`!]>N ~[1s{<0.W>');
+define('AUTH_KEY',         ' R5|N8/U}V8mx72&C4<V/;tQ)Fgd#A]k8FjSX!m]gp%0z?=dg:vlh{2v=5A=Z)=n');
+define('SECURE_AUTH_KEY',  'P<*7+#{@+qP=AtS/a-F^y>4 K9Mo1Z;$tU917GuP^oJbl4G.X!iy+CKXTtp`mZ;P');
+define('LOGGED_IN_KEY',    '$j,L}=b:X5 o66UGe:_;oq0*Gg)7<eg. <Jimz*;}ycsU2s=qa{P$8FHV0FFebT]');
+define('NONCE_KEY',        ';GKg<*59h4sB&T`R6B9,mwEV|`]9w+|sGzT9GFVPO+3|]+JJ|shb)*|`}(LwRwwP');
+define('AUTH_SALT',        'N3~ZDqBry-*q43xUb|Q{qCI+b@]w^Fa;V$YCQqM_4B- aq!jXm=8|1=)fiiv59}%');
+define('SECURE_AUTH_SALT', '}5<FfR=~gY8<A4Du1-4bT)y!< Kei3l_gK*Y#kIoUcbYePl8}2-eZTzx(x{,D0,o');
+define('LOGGED_IN_SALT',   'Exs+Z%0b}nmzD4ZQp*&?.F-gdW~pJ}2Rj$~07aiDooH2pGU]o:{XmG(COx3{o|b/');
+define('NONCE_SALT',       'Ou `0Z1CHXR9H]2,)jtF_MIv@?+5flN4cRf+sy%2PpljnFTLqr-1N_{ha[G%M1Hh');
 
 
 $table_prefix = 'wp_';
