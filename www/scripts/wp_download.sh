beea8dc1e8b54d87e3669b81dc1e7dd8664800d2
Olivier Parent <olivier.parent@arteveldehs.be>
2015-12-10 07:27:55 +0100

    [WIP]

---
+++ www/scripts/wp_download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source `dirname $0`/settings
+cd `dirname $0`/..
+wp core download --path=${WP_PATH} --locale=${WP_LOCALE}
--- www/scripts/wp_install.sh
+++ www/scripts/wp_install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 source `dirname $0`/settings
-`dirname $0`/db_reset.sh
-cd `dirname $0`/..
-wp core download --path=${WP_PATH} --locale=${WP_LOCALE}
-cd ${WP_PATH}
-wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX}
+cd `dirname $0`/../${WP_PATH}
 wp core install --url=${WP_URL} --title="${COURSE}" --admin_user=${WP_USER} --admin_password=${WP_PASS} --admin_email=${WP_MAIL}
